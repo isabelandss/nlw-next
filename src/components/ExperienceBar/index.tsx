555080afb37dbd72c9ef076c70829e48b916ffba
Isabela Neri <isabelandss@gmail.com>
2021-02-23 10:15:02 -0300

    css module

--- src/components/ExperienceBar/index.tsx
+++ src/components/ExperienceBar/index.tsx
@@ -1,12 +1,12 @@
-// import styles from './index.module.scss'
+import styles from './index.module.scss'
 
 export const ExperienceBar = () => {
   return (
-    <header className="exp-bar">
+    <header className={styles.expBar}>
       <span>0 xp</span>
-      <div className="exp-bar__bar">
-        <div className="exp-bar__bar__progress" style={{ width: '50%' }} />
-        <span className="exp-bar__bar__current" style={{ left: '50%' }}>300 xp</span>
+      <div className={styles.expBar__bar}>
+        <div className={styles.expBar__bar__progress} style={{ width: '50%' }} />
+        <span className={styles.expBar__bar__current} style={{ left: '50%' }}>300 xp</span>
       </div>
       <span>600 xp</span>
     </header>
