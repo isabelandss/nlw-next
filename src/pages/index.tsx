9c0ef0b4d700fc63ae5f9741ee3655faa319255b
Isabela Neri <isabelandss@gmail.com>
2021-02-23 10:10:20 -0300

    document

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,8 @@
-import Head from 'next/head'
 import { ExperienceBar } from '../components'
 
 export default function Home() {
   return (
     <div className="container">
-      <Head>
-        <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&family=Rajdhani:wght@300;400;500;600;700&display=swap" rel="stylesheet" />
-      </Head>
       <ExperienceBar />
     </div>
   )
